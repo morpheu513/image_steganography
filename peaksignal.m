2c6956a24c30a1400012083536bc1509dafd6e06
morpheu513 <42296576+morpheu513@users.noreply.github.com>
2020-05-18 12:58:46 +0530

    Update peaksignal.m

--- peaksignal.m
+++ peaksignal.m
@@ -1,5 +1,11 @@
-I1=imread("10.jpg");
-I2=imread("stego-10.jpg");
+%% This file is to perform a simple test on the obtained results.
+%% This test is know as SSIM or Structural Similarity index. SSIM is a test which measures the quality degradation of an image.
+%% In our case it measures the degradation caused by the embedding of our message in the image.
+%% A score of -1 means that the images are structurally very differant and +1 means that the images are structurally very similar
+
+%% Importing the message
+I1=imread("10.jpg");  %% Write the path to the original image here
+I2=imread("stego-10.jpg");  %% Write path to stego-image here
 ssimval = ssim(I2,I1);
 
 ssimval
